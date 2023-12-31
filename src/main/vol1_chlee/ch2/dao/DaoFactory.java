7aac0c91592ecda5599fa4913ef38828ad2ffb31
LEECHUNGHEON <dlcndgjs32@naver.com>
2023-12-31 18:39:30 +0900

    create ch2 and deleteAll in UserDao

---
+++ src/main/vol1_chlee/ch2/dao/DaoFactory.java
@@ -0,0 +1,35 @@
+package main.vol1_chlee.ch2.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DaoFactory {
+
+    @Bean
+    public UserDao userDao(){
+        UserDao userDao = new UserDao();
+        userDao.setDataSource(dataSource());
+        return userDao;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        String url = "jdbc:mariadb://localhost:3307/toby_study?characterEncoding=UTF-8";
+        String username = "root";
+        String password = "1234";
+
+        dataSource.setDriverClass(org.mariadb.jdbc.Driver.class);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+
+    }
+
+
+}
---
+++ src/main/vol1_chlee/ch2/dao/UserDao.java
@@ -0,0 +1,69 @@
+package main.vol1_chlee.ch2.dao;
+
+import main.vol1_chlee.ch1.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException{
+        Connection con = dataSource.getConnection();
+
+        //Prepare statements 사용
+        String sql = "INSERT INTO users(id,name,password) values(?,?,?)";
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setString(1,user.getId());
+        pst.setString(2,user.getName());
+        pst.setString(3,user.getPassword());
+
+        pst.executeUpdate();
+
+        pst.close();
+        con.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException{
+        Connection con = dataSource.getConnection();
+
+        String sql = "SELECT * FROM users WHERE id=?";
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setString(1,id);
+
+        ResultSet rs = pst.executeQuery();
+        User user = new User();
+        if(rs.next()){
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+        rs.close();
+        pst.close();
+        con.close();
+
+        return user;
+    }
+
+    // 테이블 정보 삭제
+    public void deleteAll() throws SQLException{
+        Connection con = dataSource.getConnection();
+
+        String sql = "DELETE FROM users";
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.executeUpdate();
+
+        pst.close();
+        con.close();
+    }
+
+
+}
