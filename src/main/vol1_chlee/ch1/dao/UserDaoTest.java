0d62a5f254acbb797039784edf08afe78f7062b2
LEECHUNGHEON <dlcndgjs32@naver.com>
2023-12-31 16:59:49 +0900

    create UserDaoTest

---
+++ src/main/vol1_chlee/ch1/dao/UserDaoTest.java
@@ -0,0 +1,33 @@
+package main.vol1_chlee.ch1.dao;
+
+import main.vol1_chlee.ch1.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("chlee12");
+        user.setName("leech");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+
+
+    }
+}
