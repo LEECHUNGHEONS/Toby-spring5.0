f54d5d743e39c9c0f474e57d6e60676622644c6e
LEECHUNGHEON <dlcndgjs32@naver.com>
2023-12-31 16:32:35 +0900

    Create UserDao'bean in DaoFactory

--- src/main/vol1_chlee/ch1/dao/DaoFactory.java
+++ src/main/vol1_chlee/ch1/dao/DaoFactory.java
@@ -1,10 +1,18 @@
 package main.vol1_chlee.ch1.dao;
 
+import main.vol1_chlee.ch1.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DaoFactory {
+
+    @Bean
+    public UserDao userDao(){
+        UserDao userDao = new UserDao();
+        userDao.setDataSource();
+        return userDao;
+    }
 
 
 }
