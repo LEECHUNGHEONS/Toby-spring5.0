a32ea1219685ad6ca09cf7d2159db7c45b000caf
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-02-02 20:35:39 +0900

    Add Ch6- UserService Interface

--- src/main/vol1_chlee/ch6/lch/domain/UserService.java
+++ src/main/vol1_chlee/ch6/lch/domain/UserService.java
@@ -1,7 +1,7 @@
 package main.vol1_chlee.ch6.lch.domain;
 
 public interface UserService {
-// 트랜잭션과 비즈니스 로직을 분리하기 위한 메소드와 인터페이스
+// 트랜잭션과 비즈니스 로직을 분리하기 위한 메소드와 인터페이스!
 
     void add(User user);
     void upgradeLevels();
