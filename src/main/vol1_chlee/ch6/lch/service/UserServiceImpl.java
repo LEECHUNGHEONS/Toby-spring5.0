fb7734f20aff41ae60da372cb3f01c8b8d5fda33
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-02-08 01:02:02 +0900

    ãedit ch6-UserServiceImple

--- src/main/vol1_chlee/ch6/lch/service/UserServiceImpl.java
+++ src/main/vol1_chlee/ch6/lch/service/UserServiceImpl.java
@@ -1,40 +1,68 @@
 package main.vol1_chlee.ch6.lch.service;
 
-import main.vol1_chlee.ch5.lch.dao.UserDao;
-import main.vol1_chlee.ch5.lch.domain.Level;
-import main.vol1_chlee.ch5.lch.domain.User;
+import main.vol1_chlee.ch6.lch.dao.UserDao;
+import main.vol1_chlee.ch6.lch.domain.Level;
+import main.vol1_chlee.ch6.lch.domain.User;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
-public class UserServiceImpl {
+import java.util.List;
+
+public class UserServiceImpl implements UserService{
 
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
 
     private MailSender mailSender;
 
+    private UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
     public void setMailSender(MailSender dummyMailSender) {
         this.mailSender = dummyMailSender;
     }
 
-    @Override
-    public boolean canUpgradeLevel(User user) {
-        Level currentLevel = user.getLevel();
 
-        switch(currentLevel) {
-            case BASIC : return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
-            case SILVER : return (user.getRecommend() >= MIN_RECOMMEND_FOR_GOLD);
-            case GOLD : return false;
-            default : throw new IllegalArgumentException("Unknown Level : " + currentLevel);
+
+    // 사용자를 db에 추가하고 디폴트로 basic 레벨 부여
+    public void add(User user) {
+        if (user.getLevel() == null) user.setLevel(Level.BASIC);
+        userDao.add(user);
+    }
+
+    // 사용자 레벨을 업그레이드 해주는 로직
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
         }
     }
 
-    @Override
-    public void upgradeLevel(User user, UserDao userDao) {
+
+    //업그레이드가 가능할 때 업그레이드를 가능하게 끔 해주는 로직
+    protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
         sendUpgradeEMail(user);
     }
+
+    // 업그레이드가 가능한지 확인하는 로직
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        switch(currentLevel) {
+            case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+            case SILVER: return (user.getRecommend() >= MIN_RECOMMEND_FOR_GOLD);
+            case GOLD: return false;
+            default: throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+        }
+    }
+
 
     private void sendUpgradeEMail(User user) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
