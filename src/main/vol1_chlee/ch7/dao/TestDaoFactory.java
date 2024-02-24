eb05b2b4720128835828061f89f13dfc6ecda6f5
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-02-24 20:11:02 +0900

    add ch7 - dao classes

---
+++ src/main/vol1_chlee/ch7/dao/TestDaoFactory.java
@@ -0,0 +1,27 @@
+package main.vol1_chlee.ch7.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TestDaoFactory {
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(org.mariadb.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mariadb://localhost:3307/toby_study?characterEncoding=UTF-8");
+		dataSource.setUsername("root");
+		dataSource.setPassword("1234");
+
+		return dataSource;
+	}
+
+}
+
+
---
+++ src/main/vol1_chlee/ch7/dao/UserDao.java
@@ -0,0 +1,21 @@
+package main.vol1_chlee.ch7.dao;
+
+
+import main.vol1_chlee.ch7.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+	void add(User user);
+
+	Optional<User> get(String id);
+
+	List<User> getAll();
+
+	void deleteAll();
+
+	int getCount();
+	
+	public void update(User user);
+}
