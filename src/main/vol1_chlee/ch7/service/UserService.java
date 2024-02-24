92b5053d8eb756fa00cab0faf826da30a413ac3b
LEECHUNGHEON <dlcndgjs32@naver.com>
2024-02-24 20:30:44 +0900

    add ch7 - service UserService

---
+++ src/main/vol1_chlee/ch7/service/UserService.java
@@ -0,0 +1,25 @@
+package main.vol1_chlee.ch7.service;
+
+import main.vol1_chlee.ch7.domain.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@Transactional
+public interface UserService {
+	void add(User user);
+	void deleteAll();
+	void update(User user);
+	
+	@Transactional(readOnly=true)
+	Optional<User> get(String id);
+	
+	@Transactional(readOnly=true)
+	List<User> getAll();
+	
+	void upgradeLevels();
+}
